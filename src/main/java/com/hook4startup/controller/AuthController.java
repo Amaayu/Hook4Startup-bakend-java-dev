b764ae5d06e37dcfa06dde211f7cddd65c2841c9
Amaayu <namdevaniket446@gmail.com>
2025-03-19 23:25:28 +0530

    changes

--- src/main/java/com/hook4startup/controller/AuthController.java
+++ src/main/java/com/hook4startup/controller/AuthController.java
@@ -118,6 +118,7 @@ public class  AuthController {
         // ✅ Secure cookie response send karo
         ResponseCookie cookie = ResponseCookie.from("session_token", newToken.getToken())
                 .httpOnly(false)
+                .sameSite("Lax")
                 .secure(true)
                 .path("/")
                 .maxAge(7 * 24 * 60 * 60)
